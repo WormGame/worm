aaf4e3984d0475fc63eaf81c18f7ef7ac3de67ab
ehaakdl <ehaakdl@naver.com>
2021-06-05 14:01:14 +0900

    logback.xml 설정해야함 docker mysql 설치 안됨 Jsypt 되는지 확인

--- src/main/java/com/game/worm/WormApplication.java
+++ src/main/java/com/game/worm/WormApplication.java
@@ -1,9 +1,11 @@
 package com.game.worm;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EncryptablePropertySource(name = "EncryptedProperties", value = "classpath:encrypted.properties")
 public class WormApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/game/worm/websocket/StompConfig.java
@@ -0,0 +1,24 @@
+package com.game.worm.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Profile("stomp")
+@EnableWebSocketMessageBroker
+@Configuration
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/publish");
+        registry.enableSimpleBroker("/subscribe");
+    }
+}
