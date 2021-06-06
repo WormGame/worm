7246dae0e561d226e3a5ced246488637759f764a
ehaakdl <ehaakdl@naver.com>
2021-06-06 14:20:04 +0900

    gitigonore 파일에 log 파일 추가 Jasypt 확인완료 logback 확인완료 웹소켓 파악했고 적용하면됨

--- src/main/java/com/game/worm/config/JasyptConfig.java
+++ src/main/java/com/game/worm/config/JasyptConfig.java
@@ -4,16 +4,13 @@ package com.game.worm.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class JasyptConfig {
     @Bean
-    public StringEncryptor stringEncryptor() {
+    public StringEncryptor jasyptStringEncryptor() {
         String key = System.getProperty("jasypt.encryptor.password");
         String algorithm = System.getProperty("jasypt.encryptor.algorithm");
         if (key == null | algorithm == null) {
@@ -21,17 +18,12 @@ public class JasyptConfig {
         }
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(key); //암호화에 사용할 키 -> 중요
-        config.setAlgorithm(algorithm); //사용할 알고리즘
+        config.setPassword(key);
+        config.setAlgorithm(algorithm);
         config.setKeyObtentionIterations("1000");
-        config.setAlgorithm("PBEWithMD5AndDES");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
         config.setPoolSize("1");
         encryptor.setConfig(config);
-
-        System.out.println("URL:" + encryptor.encrypt("jdbc:mysql://localhost:3308/worm?serverTimezone=Asia/Seoul&characterEncoding=UTF-8")
-        +"passswd:"+encryptor.encrypt("1234")
-        +"ID:" +encryptor.encrypt("root"));
         return encryptor;
     }
 }
