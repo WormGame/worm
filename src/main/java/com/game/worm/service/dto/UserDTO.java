06d96f8e0a745691f6d73434ba450db54a2b9530
ehaak <ehaakdl@naver.com>
2021-10-30 15:46:27 +0900

    [fix] 누락 데이터 추가

---
+++ src/main/java/com/game/worm/service/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.game.worm.service.dto;
+
+import lombok.Data;
+
+
+@Data
+public class UserDTO {
+    private long userNo;
+    private String userId;
+    private String userPasswd;
+}
