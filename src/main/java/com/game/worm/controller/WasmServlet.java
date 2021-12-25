2f9021f1a8439a58d9132f2ecea04f8ecf506083
ehaak <ehaakdl@naver.com>
2021-12-25 14:33:45 +0900

    [feat] - unity build 파일 연동했음   - 움직이는거 까지는 만듬
    문제
    - wasm에 대한 응답헤더가 없다고 뜸 에러 메시지 감추는 방법  필요

--- src/main/java/com/game/worm/controller/WasmServlet.java
+++ src/main/java/com/game/worm/controller/WasmServlet.java
@@ -7,9 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/data/unity/Build.wasm")	//Servlet Mapping 자동 설정
+@WebServlet(urlPatterns = {"/unity/Build/build.data.br", "/unity/Build/build.framework.js.br"
+        , "/unity/Build/build.wasm.br"})	//Servlet Mapping 자동 설정
 public class WasmServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/wasm");	// MIME TYPE
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        resp.addHeader("Content-Encoding","br");	// MIME TYPE
+        resp.setContentType("application/javascript");
     }
 }
