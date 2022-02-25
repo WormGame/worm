64c51a45252b2f2195880228cc1906af70772133
ehaak <ehaakdl@naver.com>
2022-02-25 23:41:03 +0900

    fix:  상수 이름/패키지 경로 조정

---
+++ src/main/java/com/game/worm/etc/config/JasyptConfig.java
@@ -0,0 +1,30 @@
+package com.game.worm.etc.define.config;
+
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class JasyptConfig {
+    @Bean
+    public StringEncryptor jasyptStringEncryptor() throws Exception {
+        String key = System.getenv("JASYPT_PASSWD");
+        String algorithm = "PBEWithMD5AndDES";
+
+        if (key == null) throw new Exception("key가 환경변수에 없다");
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(key);
+        config.setAlgorithm(algorithm);
+        config.setKeyObtentionIterations("1000");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setPoolSize("1");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
